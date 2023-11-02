4e652f5ed7425bd160d78de9a0835900688e206f
JUYOUNG KIM <101981552+kimju0@users.noreply.github.com>
2023-11-02 09:05:22 +0900

    [Gold II] Title: 저울, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Gold/2437. 저울/저울.c
+++ 백준/Gold/2437. 저울/저울.c
@@ -1,23 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-int n, arr[1000];
-long long sum=1;
-int compare(const void*first,const void*second) {
-	if (*(int*)first > *(int*)second)return 1;
-	else if (*(int*)first < *(int*)second)return -1;
-	return 0;
-}
+int n, a[1000], s=1;
+int c(const void*f,const void*s){return *(int*)f-*(int*)s;}
 int main() {
 	scanf("%d", &n);
-	for (int i = 0; i < n; i++)scanf("%d", &arr[i]);
-	qsort(arr, n, sizeof(int), compare);
-	if (arr[0] != 1) {
+	for (int i = 0; i < n; i++)scanf("%d", &a[i]);
+	qsort(a, n, 4, c);
+	if (a[0] != 1) {
 		printf("%d", 1);
 		return 0;
 	}
 	for (int i = 1; i < n; i++) {
-		if (arr[i] > sum + 1)break;
-		sum += arr[i];
+		if (a[i] > s + 1)break;
+		s += a[i];
 	}
-	printf("%d", sum + 1);
+	printf("%d", s + 1);
 }
