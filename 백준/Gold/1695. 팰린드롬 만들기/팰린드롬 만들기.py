ac05fe8f40caf4a182a75fe824f40e792f8bd2aa
JUYOUNG KIM <101981552+kimju0@users.noreply.github.com>
2024-01-29 18:20:13 +0900

    [Gold III] Title: 팰린드롬 만들기, Time: 5520 ms, Memory: 31696 KB -BaekjoonHub

---
+++ 백준/Gold/1695. 팰린드롬 만들기/팰린드롬 만들기.py
@@ -0,0 +1,15 @@
+from sys import stdin
+import copy
+
+input = stdin.readline
+
+N = int(input())
+arr = list(map(int, input().split()))
+DP = [0] * N
+for i in range(1, N):
+    temp = DP[:i + 1]
+    for j in range(i - 1, -1, -1):
+        DP[j] = min(DP[j], DP[j + 1]) + 1
+        if arr[i] == arr[j] and DP[j] > temp[j + 1]:
+            DP[j] = temp[j + 1]
+print(DP[0])
