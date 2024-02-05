93ffd094a795cd08b37271e6f0acda9ebcb2f8e4
JUYOUNG KIM <101981552+kimju0@users.noreply.github.com>
2024-02-05 09:49:47 +0900

    [Platinum V] Title: 팰린드롬 개수 구하기 (Large), Time: 8 ms, Memory: 5932 KB -BaekjoonHub

---
+++ 백준/Platinum/14517. 팰린드롬 개수 구하기 （Large）/팰린드롬 개수 구하기 （Large）.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+
+#define M 10007
+using namespace std;
+string S;
+int DP[1000][1000];
+
+int main() {
+    cin >> S;
+    for (int i = 0; i < S.size(); i++)
+        DP[i][i] = 1;
+    for (int i = 0; i < S.size() - 1; i++)
+        DP[i][i + 1] = S[i] == S[i + 1] ? 3 : 2;
+    for (int i = 2; i < S.size(); i++) {
+        for (int j = 0; j < S.size() - i; j++) {
+            DP[j][j + i] = (DP[j + 1][j + i] + DP[j][j + i - 1] + 1) % M;
+            if (S[j] != S[j + i]) {
+                DP[j][j + i] %= M;
+                DP[j][j + i] -= (DP[j + 1][j + i - 1] + 1) % M;
+            }
+            if (DP[j][j + i] < 0) DP[j][j + i] += M;
+            DP[j][j + i] %= M;
+        }
+    }
+    cout << DP[0][S.size() - 1];
+}
