3238436d70096c6007464a5cf1f2e82306898353
JUYOUNG KIM <101981552+kimju0@users.noreply.github.com>
2024-05-14 21:14:43 +0900

    [Platinum V] Title: 오아시스 재결합, Time: 320 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+stack<pair<int, int>> s;
+long long N, res, inp[500000];
+
+int main() {
+    cin >> N;
+    for (int i = 0; i < N; i++) cin >> inp[i];
+    for (int i = 0; i < N; i++) {
+        if (s.empty())
+            s.emplace(inp[i], 1);
+        else if (inp[i] > s.top().first) {
+            res++;
+            i--;
+            if (s.top().second == 1)
+                s.pop();
+            else
+                s.top().second--;
+        } else if (inp[i] == s.top().first) {
+            res += s.top().second;
+            s.top().second++;
+            if (s.size() > 1)
+                res++;
+        } else {
+            res++;
+            s.emplace(inp[i], 1);
+        }
+    }
+    cout << res;
+}
