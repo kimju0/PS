1f8ccac1818f79878497b29e04227c06f13f4c04
JUYOUNG KIM <101981552+kimju0@users.noreply.github.com>
2024-08-15 23:50:25 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,18 @@
+a = int(input())
+for _ in range(a):
+    b = input()
+    if b == "Algorithm":
+        print("204")
+    elif b == "DataAnalysis":
+        print("207")
+    elif b == "ArtificialIntelligence":
+        print("302")
+    elif b == "CyberSecurity":
+        print("B101")
+    elif b == "Network":
+        print("303")
+    elif b == "Startup":
+        print("501")
+    elif b == "TestStrategy":
+        print("105")
+    
